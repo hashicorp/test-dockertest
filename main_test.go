1c655a158a13de5b3d2de4f1c47575507c6f0177
nathan r. hruby <nhruby@gmail.com>
2019-02-04 13:48:02 -0700

    fix error msg

--- main_test.go
+++ main_test.go
@@ -34,7 +34,7 @@ func TestMain(m *testing.M) {
         }
         return db.Ping()
     }); err != nil {
-        log.Fatalf("Could not connect to docker: %s", err)
+        log.Fatalf("Could not connect to mysql: %s", err)
     }
 
     code := m.Run()
